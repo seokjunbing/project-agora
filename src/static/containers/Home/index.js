88fff748e24d5da9dba99b8bd28cb73e38f86a97
Jasper Bingham <binghamjasper@gmail.com>
2016-10-13 18:41:38 -0400

    added more menu features and welcome message

--- src/static/containers/Home/index.js
+++ src/static/containers/Home/index.js
@@ -19,12 +19,29 @@ class HomeView extends React.Component {
 
     render() {
         return (
-            <div className="ui top fixed menu">
-              <div className="item">
-                <h2>AGORA</h2>
-              </div>
-              <a className="item">Listings</a>
-              <a className="item">About</a>
+            <div>
+                <div className="ui top fixed menu">
+                  <div className="item">
+                    <h2>AGORA</h2>
+                  </div>
+                  <a className="item">Listings</a>
+                  <a className="item">About</a>
+                  <div className="item">
+                    <div className="ui category search item">
+                      <div className="ui transparent icon input">
+                        <input className="prompt" type="text" placeholder="Search listings..."/>
+                        <i className="search link icon"></i>
+                      </div>
+                      <div className="results"></div>
+                    </div>
+                  </div>
+                  <div className="right item">
+                    <div className="ui primary button">Log In</div>
+                  </div>
+                </div>
+                <div className="ui raised segment">
+                  <p>Welcome to Agora, the Dartmouth student marketplace.</p>
+                </div>
             </div>
         );
     }
