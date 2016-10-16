abbf9b01a2db28ad11ddb77f816ef103809b4557
Audyn <a.17@dartmouth.edu>
2016-10-16 18:56:48 -0400

    Made Account page scaffold

--- src/static/containers/index.js
+++ src/static/containers/index.js
@@ -2,3 +2,4 @@ export HomeView from './Home/index';
 export ListingView from './Listing/index';
 export ProtectedView from './Protected/index';
 export NotFoundView from './NotFound/index';
+export AccountView from './Account/index';
--- src/static/routes.js
+++ src/static/routes.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './app';
-import { HomeView, ListingView, ProtectedView, NotFoundView } from './containers';
+import { HomeView, ListingView, AccountView, ProtectedView, NotFoundView } from './containers';
 import { requireAuthentication } from './utils/requireAuthentication';
 
 export default(
     <Route path="/" component={App}>
         <IndexRoute component={HomeView}/>
         <Route path="listing" component={ListingView}/>
+        <Route path="account" component={AccountView}/>
         <Route path="protected" component={requireAuthentication(ProtectedView)}/>
         <Route path="*" component={NotFoundView}/>
     </Route>
