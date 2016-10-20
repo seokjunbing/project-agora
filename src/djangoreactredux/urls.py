4f754f2545028096b8f075eda0b91e7bdbfa00b3
Jasper Bingham <binghamjasper@gmail.com>
2016-10-20 17:41:15 -0400

    fixed index view

--- src/djangoreactredux/urls.py
+++ src/djangoreactredux/urls.py
@@ -2,10 +2,11 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.views.decorators.cache import cache_page
 from django.contrib import admin
+from agora.views import IndexView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
     # catch all others because of how history is handled by react router - cache this page because it will never change
-    #url(r'', cache_page(settings.PAGE_CACHE_SECONDS)(base_views.IndexView.as_view()), name='index'),
+    url(r'', cache_page(settings.PAGE_CACHE_SECONDS)(IndexView.as_view()), name='index'),
 ]
