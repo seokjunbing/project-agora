58cba3fbf25b3c183153b5c8bce90c1369f54f6c
orzsikodon <odon.orzsik.17@dartmouth.edu>
2016-11-01 23:55:18 -0400

    listings reducer and action implemented

--- src/static/containers/index.js
+++ src/static/containers/index.js
@@ -3,3 +3,4 @@ export ListingView from './Listing/index';
 export ProtectedView from './Protected/index';
 export NotFoundView from './NotFound/index';
 export AccountView from './Account/index';
+export AboutView from './About/index';
--- src/static/routes.js
+++ src/static/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './app';
-import { HomeView, ListingView, AccountView, ProtectedView, NotFoundView } from './containers';
+import { HomeView, ListingView, AccountView, AboutView, ProtectedView, NotFoundView } from './containers';
 import { requireAuthentication } from './utils/requireAuthentication';
 
 export default(
@@ -9,6 +9,7 @@ export default(
         <IndexRoute component={HomeView}/>
         <Route path="listing" component={ListingView}/>
         <Route path="account" component={AccountView}/>
+        <Route path="about" component={AboutView}/>
         <Route path="protected" component={requireAuthentication(ProtectedView)}/>
     </Route>
 );
