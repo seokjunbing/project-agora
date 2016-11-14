8243c0ab345697400b499239ff34685813a5ff5e
Audyn <a.17@dartmouth.edu>
2016-11-13 21:45:59 -0500

    fixing routes file

--- src/static/routes.js
+++ src/static/routes.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './app';
-import { HomeView, ListingView, AccountView, AboutView, ProtectedView, NotFoundView, VerifyEmailView } from './containers';
+import { HomeView, ListingView, AccountView, AboutView, ProtectedView, NotFoundView, VerifyEmailView, CreateListingView } from './containers';
 import { requireAuthentication } from './utils/requireAuthentication';
 
 export default(
     <Route path="/" component={App}>
         <IndexRoute component={HomeView}/>
         <Route path="verify" component={VerifyEmailView}/>
+        <Route path="createlisting" component={CreateListingView}/>
         <Route path="listing" component={ListingView}/>
         <Route path="account" component={AccountView}/>
         <Route path="about" component={AboutView}/>
