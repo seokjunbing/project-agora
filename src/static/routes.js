efd6a59c58f4cffd87d72c1c3fd9e9fcd786f301
Audyn <a.17@dartmouth.edu>
2016-11-13 20:03:01 -0500

    merge conflicts 3

--- src/static/routes.js
+++ src/static/routes.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './app';
-
-import { HomeView, ListingView, AccountView, AboutView, ProtectedView, NotFoundView } from './containers';
-
+import { HomeView, ListingView, AccountView, AboutView, ProtectedView, NotFoundView, VerifyEmailView } from './containers';
 import { requireAuthentication } from './utils/requireAuthentication';
 
 export default(
     <Route path="/" component={App}>
         <IndexRoute component={HomeView}/>
+        <Route path="verify" component={ListingView}/>
         <Route path="listing" component={ListingView}/>
         <Route path="account" component={AccountView}/>
         <Route path="about" component={AboutView}/>
