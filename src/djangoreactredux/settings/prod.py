273790f3f51b14e8418888f72fa961b08eed9222
Jasper Bingham <binghamjasper@gmail.com>
2016-11-15 12:07:07 -0500

    taking out django rest logger stuff

--- src/djangoreactredux/settings/prod.py
+++ src/djangoreactredux/settings/prod.py
@@ -13,50 +13,50 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config()
 
-REST_FRAMEWORK = {
-    'EXCEPTION_HANDLER': 'django_rest_logger.handlers.rest_exception_handler'  # NOQA (ignore all errors on this line)
-}
+# REST_FRAMEWORK = {
+#     # 'EXCEPTION_HANDLER': 'django_rest_logger.handlers.rest_exception_handler'  # NOQA (ignore all errors on this line)
+# }
 
 WSGI_APPLICATION = 'djangoreactredux.wsgi.application'
 
 # Note turned off sentry. We're going to use the default debugging
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'root': {
-        'level': 'DEBUG',
-        'handlers': ['django_rest_logger_handler'],
-    },
-    'formatters': {
-        'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s '
-                      '%(process)d %(thread)d %(message)s'
-        },
-    },
-    'handlers': {
-        'django_rest_logger_handler': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
-        }
-    },
-    'loggers': {
-        'django.db.backends': {
-            'level': 'ERROR',
-            'handlers': ['django_rest_logger_handler'],
-            'propagate': False,
-        },
-        'django_rest_logger': {
-            'level': 'DEBUG',
-            'handlers': ['django_rest_logger_handler'],
-            'propagate': False,
-        },
-    },
-}
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': True,
+#     'root': {
+#         'level': 'DEBUG',
+#         'handlers': ['django_rest_logger_handler'],
+#     },
+#     'formatters': {
+#         'verbose': {
+#             'format': '%(levelname)s %(asctime)s %(module)s '
+#                       '%(process)d %(thread)d %(message)s'
+#         },
+#     },
+#     'handlers': {
+#         'django_rest_logger_handler': {
+#             'level': 'DEBUG',
+#             'class': 'logging.StreamHandler',
+#             'formatter': 'verbose'
+#         }
+#     },
+#     'loggers': {
+#         'django.db.backends': {
+#             'level': 'ERROR',
+#             'handlers': ['django_rest_logger_handler'],
+#             'propagate': False,
+#         },
+#         'django_rest_logger': {
+#             'level': 'DEBUG',
+#             'handlers': ['django_rest_logger_handler'],
+#             'propagate': False,
+#         },
+#     },
+# }
 
-DEFAULT_LOGGER = 'django_rest_logger'
+# DEFAULT_LOGGER = 'django_rest_logger'
 
-LOGGER_EXCEPTION = DEFAULT_LOGGER
-LOGGER_ERROR = DEFAULT_LOGGER
-LOGGER_WARNING = DEFAULT_LOGGER
+# LOGGER_EXCEPTION = DEFAULT_LOGGER
+# LOGGER_ERROR = DEFAULT_LOGGER
+# LOGGER_WARNING = DEFAULT_LOGGER
