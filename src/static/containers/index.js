dbc34009b8aab2332ca262b20234a5cccf1fdfde
Jasper Bingham <binghamjasper@gmail.com>
2017-01-15 14:32:25 -0500

    Made static version of messaging UI

--- src/static/containers/index.js
+++ src/static/containers/index.js
@@ -8,3 +8,4 @@ export CreateListingView from './CreateListing/index';
 
 export AboutView from './About/index';
 export VerifyEmailView from './VerifyEmail/index';
+export MessagesView from './Messages/index';
