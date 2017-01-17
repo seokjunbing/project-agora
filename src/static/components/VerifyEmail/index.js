e0214542c7046f3639c5cc7abfaf3a01d7c88f76
orzsikodon <odon.orzsik.17@dartmouth.edu>
2017-01-17 18:08:27 -0500

    absolute layout fixed

--- src/static/components/VerifyEmail/index.js
+++ src/static/components/VerifyEmail/index.js
@@ -7,47 +7,28 @@ class VerifyEmail extends React.Component {
     }
 
     render() {
-      var style_head = {
+      var style_centered = {
         textAlign: 'center',
       }
 
-      var style_input = {
-        display: 'block',
+      var email_size = {
         textAlign: 'center',
-        marginTop: '45px',
+        width: '150px'
       }
 
-      var label_email = {
-        marginTop: '30px',
-        marginLeft: '170px'
+      var style_block = {
+        display: 'block'
       }
 
-      var label_email_style = {
-        width: '150px',
-        marginLeft: '170px'
+      var vertical_offset = {
+        marginTop: '30px'
       }
 
-      var label_username_style = {
-        width: '279px',
-        marginLeft: '170px'
-      }
-
-      var label_password = {
-        marginTop: '30px',
-        marginRight: '219px'
-      }
-
-      var label_username_login = {
+      var field_length = {
         width: '279px'
       }
 
       var button_style = {
-        marginTop: '30px',
-        marginLeft: '243',
-        textAlign: 'center'
-      }
-
-      var button_style_login = {
         marginTop: '30px',
         textAlign: 'center'
       }
@@ -56,34 +37,34 @@ class VerifyEmail extends React.Component {
 
           <Grid columns={2} relaxed>
             <Grid.Column>
-              <Container>
-                <Header style={style_head}>
+              <Container textAlign='center'>
+                <Header style={style_centered}>
                   Sign up for Agora:
                 </Header>
                 <Form>
                   <Form.Field>
-                    <label style={label_email}>Email</label>
-                    <Input style= {label_email_style} labelPosition='right'>
-                      <input style={style_head} placeholder='your.name' type='text' />
+                    <label style={vertical_offset}>Email</label>
+                    <Input style= {style_block} labelPosition='right'>
+                      <input style={email_size} placeholder='your.name' type='text' />
                       <Label>@dartmouth.edu</Label>
                     </Input>
                   </Form.Field>
                   <Form.Field>
-                    <label style={label_email}>Username</label>
-                    <Input style= {label_username_style}>
-                      <input style={style_head} placeholder='DartMan' type='text' />
+                    <label style={vertical_offset}>Username</label>
+                    <Input style= {field_length}>
+                      <input style={style_centered} placeholder='DartMan' type='text' />
                     </Input>
                   </Form.Field>
                   <Form.Field>
-                    <label style={label_email}>Password</label>
-                    <Input style= {label_username_style}>
-                      <input style={style_head} placeholder='XXXXXXXXXXXXX' type='password' />
+                    <label style={vertical_offset}>Password</label>
+                    <Input style= {field_length}>
+                      <input style={style_centered} placeholder='XXXXXXXXXXXXX' type='password' />
                     </Input>
                   </Form.Field>
                   <Form.Field>
-                    <label style={label_email}>Verify Password</label>
-                    <Input style= {label_username_style}>
-                      <input style={style_head} placeholder='XXXXXXXXXXXXX' type='password' />
+                    <label style={vertical_offset}>Verify Password</label>
+                    <Input style= {field_length}>
+                      <input style={style_centered} placeholder='XXXXXXXXXXXXX' type='password' />
                     </Input>
                   </Form.Field>
                   <Button style={button_style} color='teal' content='Create Account' />
@@ -93,23 +74,23 @@ class VerifyEmail extends React.Component {
 
             <Grid.Column>
               <Container textAlign='center'>
-              <Header style={style_head}>
+              <Header style={style_centered}>
                 Log in:
               </Header>
                 <Form>
                   <Form.Field>
-                    <label style={label_password}>Username</label>
-                    <Input style= {label_username_login}>
-                      <input style={style_head} placeholder='DartMan' type='text' />
+                    <label style={vertical_offset}>Username</label>
+                    <Input style= {field_length}>
+                      <input style={style_centered} placeholder='DartMan' type='text' />
                     </Input>
                   </Form.Field>
                   <Form.Field>
-                    <label style={label_password}>Password</label>
-                    <Input style= {label_username_login}>
-                      <input style={style_head} placeholder='XXXXXXXXXXXXX' type='password' />
+                    <label style={vertical_offset}>Password</label>
+                    <Input style= {field_length}>
+                      <input style={style_centered} placeholder='XXXXXXXXXXXXX' type='password' />
                     </Input>
                   </Form.Field>
-                  <Button style={button_style_login} color='teal' content='log in' />
+                  <Button style={button_style} color='teal' content='log in' />
                 </Form>
               </Container>
             </Grid.Column>
