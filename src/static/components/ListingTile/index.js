4c87af69692e4f86c12414ba45fcc2c5612ad02f
Jasper Bingham <binghamjasper@gmail.com>
2017-02-07 15:46:26 -0500

    added more info to modal

--- src/static/components/ListingTile/index.js
+++ src/static/components/ListingTile/index.js
@@ -57,6 +57,10 @@ class ListingTile extends React.Component {
             minHeight: '200px',
             maxHeight: '200px',
         }
+        var style5 = {
+            display: 'inline-block',
+            marginRight: '10%',
+        }
         return (
             <Card style={style1}>
                 <Image style={style4} src={this.props.images ? this.props.images[0] : ''}/>
@@ -67,14 +71,39 @@ class ListingTile extends React.Component {
                     <Modal.Content>
                       <Modal.Description>
                         <Header as='h2'>{this.props.title}</Header>
-                        <Header sub>Price</Header>
-                        <span>${this.props.price}{this.props.extraPriceInfo}</span>
+                        <div style={style5}>
+                            <Header sub>Price</Header>
+                            <span>${this.props.price}{this.props.extraPriceInfo}</span>
+                        </div>
+                        <div style={style5}>
+                            <Header sub>Category</Header>
+                            <span>{this.props.category}</span>
+                        </div>
+                        <div style={style5}>
+                            <Header sub>Seller</Header>
+                            <span>{this.props.author}</span>
+                        </div>
                         <Divider/>
                         <p>{this.props.description}</p>
                       </Modal.Description>
                     </Modal.Content>
                     <Modal.Actions>
-                        <Button color='teal' icon='mail outline' labelPosition='right' content='Contact Seller' />
+                        {this.props.user_id &&
+                            <Modal trigger={<Button color='teal' icon='mail outline' labelPosition='right' content='Contact Seller' />} basic>
+                            <Modal.Header>
+                                <h1>Contact Seller</h1>
+                                <h4>Write your first message below.</h4>
+                            </Modal.Header>
+                            <Modal.Content>
+                              <Modal.Description>
+                                  <Form onSubmit={this.handleSubmit.bind(this)}>
+                                      <Form.Input name='message' placeholder='Write a message...' onChange={this.updateMessage.bind(this)} action={<Button color='teal' type='submit' id='messageSubmit'>Send</Button>}/>
+                                  </Form>
+                              </Modal.Description>
+                            </Modal.Content>
+                            <Modal.Actions>
+                              </Modal.Actions>
+                          </Modal>}
                       </Modal.Actions>
                   </Modal>
                 <Card.Content extra>
