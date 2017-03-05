a2495594552ab8b81271ac3f439b992f6b13c043
Jasper Bingham <binghamjasper@gmail.com>
2017-03-05 14:38:02 -0500

    prelim version of listing modal

---
+++ src/static/components/ListingModal/ImageDisplay/index.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Segment, Image, Grid, Label, Progress } from 'semantic-ui-react';
+import * as postActionCreators from '../../../actions/postlisting';
+import * as catActionCreators from '../../../actions/categories';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import CategoryDropdown from '../../CategoryDropdown';
+import ReactS3Uploader from 'react-s3-uploader';
+
+import { Button, Checkbox, Form, Input, Message, Radio, Select, TextArea, Container, Dropdown, Modal, Header, Icon } from 'semantic-ui-react';
+
+const image_title_length = 100;
+
+class ImageDisplay extends Component {
+
+
+  constructor(props) {
+    super(props);
+  }
+
+
+
+  render() {
+    return (
+      <Segment basic style={{padding : 5, border : 0}}>
+        <Image size='small' src={this.props.imageurl}/>
+        <Container fluid ><span>{this.props.caption}</span></Container>
+      </Segment>
+    )
+  }
+}
+
+
+export default (ImageDisplay);
---
+++ src/static/components/MessageModal/index.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { Modal, Form, Button } from 'semantic-ui-react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as actionCreators from '../../actions/messaging';
+
+class MessageModal extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    updateMessage = (e) => {
+        this.setState({
+            text: e.target.value,
+        });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        var users = [];
+        users.push(this.props.user_id);
+        users.push(this.props.author_id);
+        this.setState({
+            listing: this.props.listingId,
+            users: users,
+            user: this.props.user_id,
+        }, function() {
+            this.props.actions.createConversation(this.state);
+            window.location = '/messaging';
+        });
+    }
+
+    render() {
+        return (
+            <Modal trigger={this.props.trigger} basic>
+                <Modal.Header>
+                    <h1>Contact Seller</h1>
+                    <h4>Write your message below.</h4>
+                </Modal.Header>
+                <Modal.Content>
+                  <Modal.Description>
+                      <Form onSubmit={this.handleSubmit.bind(this)}>
+                          <Form.Input name='message' placeholder='Write a message...' onChange={this.updateMessage.bind(this)} action={<Button color='teal' type='submit' id='messageSubmit' disabled={(this.state && this.state.text) ? false : true}>Send</Button>}/>
+                      </Form>
+                  </Modal.Description>
+                </Modal.Content>
+                <Modal.Actions>
+                </Modal.Actions>
+            </Modal>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        user_id : state.user.user_id,
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        dispatch,
+        actions: bindActionCreators(actionCreators, dispatch)
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(MessageModal);
