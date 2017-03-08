a7d0b1d376c691b416c6b0e41e2001e0dc1dcb6e
Jasper Bingham <binghamjasper@gmail.com>
2017-03-08 17:00:17 -0500

    fixed filter url

--- src/static/selectors/filters.js
+++ src/static/selectors/filters.js
@@ -16,6 +16,6 @@ export const buildQueryString = createSelector(
         if (qs.length > 0){
             qs = qs.substring(0, qs.length-1); //chop off last "&"
         }
-        return window.location.protocol + '//' + window.location.host + '/api/listings/homepage/' + qs;
+        return window.location.protocol + '//' + window.location.host + '/api/homepage/' + qs;
     }
 );
