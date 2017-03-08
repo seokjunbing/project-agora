61b65d17094a131b3e584fe1ecd1bc83032576fd
Juan C. Torres <juan.c.torres.ramirez.17@dartmouth.edu>
2017-03-08 16:37:03 -0500

    Flagging endpoint at `/api/listings/{pk}/flag_listing/`

--- src/agora/views.py
+++ src/agora/views.py
@@ -224,13 +224,12 @@ class ListingViewSet(viewsets.ModelViewSet):
 
             # return Response(data={"detail": "Invalid input."}, status=status.HTTP_403_FORBIDDEN)
 
-    @detail_route(permission_classes=(IsAuthenticated,))
+    @detail_route(permission_classes=(IsVerified,))
     def flag_listing(self, request, pk=None):
         listing = Listing.objects.get(pk=pk)
         listing.flags += 1
         listing.save()
         return Response(data={'detail': 'Listing flagged'})
-
 
     # IsAuthenticated should suffice, as you need to be verified to create a listing. User is gotten from the auth token
     @list_route(permission_classes=(IsAuthenticated,))
